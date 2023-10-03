abb5809046f31cd942fd963b5d6b4c8321985616
Jessica Elberg <elbergjessica@gmail.com>
2023-10-03 13:47:44 -0400

    changes

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+/*
 export default function Header() {
   return (
     <header className="flex flex-row p-8 bg-transparent bg-opacity-5 shadow-lg md:full ">
@@ -25,4 +25,46 @@ export default function Header() {
       </button>
     </header>
   );
-}
+}*/
+
+
+export default function Header() {
+    return (
+        <div className='navbar bg-base-100 py-4 px-8'>
+            <div className='navbar-start'>
+                <a to={'/'} className='flex items-center gap-4 normal-case btn btn-ghost'>
+                    <img alt='Logo' className='logo w-8 md:w-12' />
+                    <h1 className='text-2xl text-bold md:text-4xl'>Book App</h1>
+                </a>
+            </div>
+            <div className='navbar-center hidden lg:flex'>
+                <ul className='menu menu-horizontal px-1'>
+                    <li>
+                        <a to={'/'}>Libros Disponibles</a>
+                    </li>
+                    <li>
+                        <a to={'/reading-list'}>Lista de Lectura</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='navbar-end'>
+
+                <div className='dropdown dropdown-bottom dropdown-end'>
+                    <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+                        <svg xmlns='http://www.w3.org/2000/svg' className='h-8 w-8' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h8m-8 6h16' />
+                        </svg>
+                    </label>
+                    <ul tabIndex={0} className='menu menu-sm dropdown-content z-[1] p-4 shadow bg-base-100 rounded-box w-52'>
+                        <li className='py-2'>
+                            <a to={'/'}>Libros Disponibles</a>
+                        </li>
+                        <li className='py-2'>
+                            <a to={'/reading-list'}>Lista de Lectura</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/pages/homePage/Home.js
+++ src/pages/homePage/Home.js
@@ -1,14 +1,6 @@
 import { useState } from "react";
 import useAuth from "../../hooks/useAuth.js";
-import searchApi from "../../services/search/seachApi.js";
 import { ToastContainer } from "react-toastify";
-import { Notification } from "../../components/notification/Notification.js";
-import { FILTER_TYPE } from "../../const/utils.js";
-import Filter from "../../components/utils/Filter.js";
-import ListArtist from "../../components/artists/ListArtists.js";
-import { useAlbumContext } from "../../contexts/AlbumContext.js";
-import useFilter from "../../hooks/useFilter.js";
-import ListAlbum from "../../components/albums/ListAlbum.js";
 
 import Header from "../../layouts/header.js";
 import Footer from "../../layouts/footer.js";
@@ -16,29 +8,12 @@ import Footer from "../../layouts/footer.js";
 export default function () {
   const { logout, auth } = useAuth();
   const [text, setText] = useState("");
-  const [artists, setArtists] = useState([]);
-  const [searchAlbums, setSearchAlbums] = useState([]);
-  const { albumList, albums } = useAlbumContext();
-  const { type } = useFilter();
+
 
   const exit = () => {
     logout();
   };
 
-  const search = async () => {
-    if (text === "") {
-      await Notification("Campo de búsqueda no debe estar vacio", "error");
-    } else {
-      if (type === FILTER_TYPE.album) {
-        const sAlbum = await searchApi(auth, text, type);
-        console.log(sAlbum);
-        setSearchAlbums(sAlbum.albums.items);
-      } else if (type === FILTER_TYPE.artist) {
-        const artista = await searchApi(auth, text);
-        setArtists(artista.artists.items);
-      }
-    }
-  };
 
   const handlerInput = (event) => {
     setText(event.target.value);
