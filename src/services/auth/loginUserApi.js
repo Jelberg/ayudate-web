769c38210bfa5f6307582aba909be9d2a15bd4ae
Jessica Elberg <thejessidev@gmail.com>
2023-10-08 03:04:27 -0400

    delete: components

--- src/services/auth/loginUserApi.js
+++
@@ -1,8 +0,0 @@
-const _email = process.env.REACT_APP_EMAIL;
-const _password = process.env.REACT_APP_PASSWORD;
-
-export function loginApi(email, password) {
-  if (email === _email && password === _password) {
-    return true;
-  } else return false;
-}
